51c1c7d1c3784f05f71fc028bfad27134c68d05f
sduartecor <santiagoduartecor@outlook.com>
2023-01-17 18:11:10 +0000

    Fix index

--- src/js/component/cardPersonajes.jsx
+++ src/js/component/cardPersonajes.jsx
@@ -28,16 +28,16 @@ export const CardPersonajes = () => {
 
 
     return(
-		<>		{personajes.map((item, id) =>  <div className="m-4 ms-0" key={id}>
+		<>		{personajes.map((item, index) =>  <div className="m-4 ms-0" key={index}>
 		<div className="card" style={{width: "15rem"}}>
-	  <img src={"https://starwars-visualguide.com/assets/img/characters/"+ (id+1) +".jpg"}  className="card-img-top" alt="..."/>
+	  <img src={"https://starwars-visualguide.com/assets/img/characters/"+ (index+1) +".jpg"}  className="card-img-top" alt="..."/>
 	  <div className="card-body">
 	  <h4 className="card-title">{item.name}</h4>
 				<p className="card-text">Gender: {item.gender}</p>
 				<p className="card-text">Hair color: {item.hair_color}</p>
 				<p className="card-text">Eye color: {item.eye_color}</p>
 				<div className="d-flex justify-content-between">
-				<Link className="btn btn-outline-primary" to={"/detail-people/"+ (id+1) } >Learn More!</Link>
+				<Link className="btn btn-outline-primary" to={"/detail-people/"+ (index+1) } >Learn More!</Link>
 				<Link className="btn btn-outline-warning"><i className="fa fa-heart"></i></Link>
 				</div>
 	  </div>
